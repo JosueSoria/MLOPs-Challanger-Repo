b550a24e0c909e618ad79c9e27c9fa670a77cae4
josue.soria <josue.soria@derevo.com.mx>
2023-07-27 14:12:32 -0600

    test adjust

--- src/model/train.py
+++ src/model/train.py
@@ -22,8 +22,9 @@ def main(args):
 
 
 def split_data(df):
-    X, y = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure', 'TricepsThickness', 'SerumInsulin', 'BMI', 'DiabetesPedigree',
-                'Age']].values, df['Diabetic'].values
+    X, y = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure', 
+            'TricepsThickness', 'SerumInsulin', 'BMI', 'DiabetesPedigree',
+            'Age']].values, df['Diabetic'].values
     len(X)
     print(np.unique(y, return_counts=True))
     return train_test_split(X, y, test_size=0.30, random_state=0)
@@ -69,4 +70,3 @@ if __name__ == "__main__":
     # add space in logs
     print("*" * 60)
     print("\n\n")
-    
